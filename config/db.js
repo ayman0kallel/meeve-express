4ea4e918b74f5362fb7c9d3f79e5920cb60d7e72
ayman0kallel <ayman0kallel@gmail.com>
2024-01-09 15:09:49 +0100

    fix host

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'mariasegovia.com',
+  host: 'localhost',
   user: 'u169578222_meeve',
   password: 'MeeveCIM1@',
   database: 'u169578222_meeve',
